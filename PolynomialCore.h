035705fb6eab2b01addb36f95bd0e9701cc03965
ZORTIX <toarev7@gmail.com>
2026-04-12 18:37:14 +0300

    add constructors

--- PolynomialCore.h
+++ PolynomialCore.h
@@ -5,8 +5,36 @@
 
 
 template <class T>
-Polynomial<T>::Polynomial() : coefficients(new MutableArraySequence<T>()) {
-    coefficients->Append(T(0));
+Polynomial<T>::Polynomial() {
+    coefficients = new MutableArraySequence<T>();
+    coefficients->append(T(0));
+}
+
+template <class T>
+Polynomial<T>::Polynomial(const Polynomial<T> &other) {
+    Normalize();
+}
+
+template <class T>
+Polynomial<T>::Polynomial(const Polynomial<T> &other) {
+    coefficients = other.coefficients->copy();
+}
+
+template <class T>
+Polynomial<T>::Polynomial(Polynomial<T>&& other) noexcept: coefficients(other.coefficients) {
+    other.coefficients = nullptr;
+}
+
+template <class T>
+Polynomial<T>::~Polynomial() {
+    delete coefficients;
+}
+
+template <class T>
+void Polynomial<T>::Normalize() {
+    while (coefficients->getLength() > 1 && coefficients->getLast() == T(0)) {
+        coefficients->removeLast();
+    }
 }
 
 
